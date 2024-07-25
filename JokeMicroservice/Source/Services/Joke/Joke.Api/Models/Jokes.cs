ed3f867f10213899e3a0fa029f53143accfd4cbf
James <sep7696@gmail.com>
2024-07-25 22:45:07 +0330

    Feate Ignore .vs

---
+++ JokeMicroservice/Source/Services/Joke/Joke.Api/Models/Jokes.cs
@@ -0,0 +1,12 @@
+﻿namespace Joke.Api.Models
+{
+    public class Jokes
+    {
+        public Guid JokeId { get; set; }
+        public int JokeTile { get; set; } 
+        public string JokeText { get; set; } = default!;
+        public DateTime JokeCreateionDate { get; set; } = DateTime.Now;
+        public string UserId { get; set; } = default!;
+
+    }
+}
